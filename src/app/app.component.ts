5a58151f1cee017b0e176321e7e7351034ac1a15
Student <example@example.com>
2019-06-10 11:34:31 -0700

    added editable forms and done button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Task } from './models/task.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  currentFocus: string = 'Angular Homework';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+  tasks: Task[] = [
+    new Task('Finish weekend Angular homework for Epicodus course', 3),
+    new Task('Begin brainstorming possible JavaScript group projects', 2),
+    new Task('Add README file to last few Angular repos on GitHub', 1)
+  ];
+  // selectedTask: Task = this.tasks[0];
+  selectedTask = null;
+
+  editTask(clickedTask) {
+    this.selectedTask = clickedTask;
+  }
+  priorityColor(currentTask){
+    if(currentTask.priority === 3){
+      return "bg-danger";
+    } else if(currentTask.priority === 2){
+      return "bg-warning";
+    } else {
+      return "bg-info";
+    }
+  }
+  finishedEditing() {
+    this.selectedTask = null;
+  }
 }
+
